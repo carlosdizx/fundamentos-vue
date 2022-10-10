0e205e0cabb425bc90f7171daf676d839fefab07
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-10-10 18:30:49 -0500

    Preparando proyecto

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    open: true,
+    compress: true,
+    https: true,
+    hot: true,
+    allowedHosts: "all",
+    historyApiFallback: true,
+    port: 8080,
+    host: "localhost",
+    static: {
+      directory: path.resolve(__dirname, "./app"),
+      staticOptions: {},
+      watch: true,
+    },
+  },
 });
